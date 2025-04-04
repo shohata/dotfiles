82a5b0066b0244f8c94a5570e8cf807afd53be53
Soichriro Shohata <so.shohata@gmail.com>
2025-04-04 16:31:38 +0900

    modify neovim config

--- config/nvim/lua/plugins/util.lua
+++ config/nvim/lua/plugins/util.lua
@@ -4,11 +4,27 @@
 
 return {
     {
-        "dstein64/vim-startuptime",
-        cmd = "StartupTime",
-        config = function()
-            vim.g.startuptime_tries = 10
-        end,
+        "snacks.nvim",
+        opts = {
+            bigfile = { enabled = true },
+            quickfile = { enabled = true },
+            terminal = {
+            win = {
+                keys = {
+                nav_h = { "<C-h>", function() return term_nav("h") end, desc = "Go to Left Window", expr = true, mode = "t" },
+                nav_j = { "<C-j>", function() return term_nav("j") end, desc = "Go to Lower Window", expr = true, mode = "t" },
+                nav_k = { "<C-k>", function() return term_nav("k") end, desc = "Go to Upper Window", expr = true, mode = "t" },
+                nav_l = { "<C-l>", function() return term_nav("l") end, desc = "Go to Right Window", expr = true, mode = "t" },
+                },
+            },
+            },
+        },
+        -- stylua: ignore
+        keys = {
+            { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+            { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+            { "<leader>dps", function() Snacks.profiler.scratch() end, desc = "Profiler Scratch Buffer" },
+        },
     },
     {
         "folke/persistence.nvim",
